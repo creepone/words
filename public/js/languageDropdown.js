dbafc64bc635abd33e250bcbb8b95d4ad923683a
Tomas Vana <tomas@Tomas-Vanas-iMac.local>
2013-04-15 19:49:21 +0200

    language list completed

--- public/js/languageDropdown.js
+++ public/js/languageDropdown.js
@@ -1,8 +1,5 @@
 (function($){
 
-	var languagesPath = "/images/flags/";
-
-
 	$.fn.languageDropdown = function() {
 		if (arguments.length == 0) {
 			// create language dropdown hosted in the given element(s)
@@ -10,22 +7,21 @@
     			create.call($(this));
     		});
 		}
-		else if (arguments.length == 1 && typeof arguments[0] == "string") {
+		else if (typeof arguments[0] == "string") {
 			switch (arguments[0]) {
 				case "selection":
-					var iso = this.data("selected");
-					if (iso in languages)
-						return $.extend({}, languages[iso], { iso: iso });
-					return;
-			}
-		}
-		else if (arguments.length == 2 && typeof arguments[0] == "string") {
-			switch (arguments[0]) {
-				case "select":
-					var iso = arguments[1];
-					if (typeof iso == "string")
-						select.call(this, iso);
-					return;
+					if (arguments.length == 1) {
+						var iso = this.data("selected");
+						if (iso in languages)
+							return $.extend({}, languages[iso], { iso: iso });
+						return;
+					}
+					else if (arguments.length == 2) {
+						var iso = arguments[1];
+						if (typeof iso == "string")
+							select.call(this, iso);
+						return;
+					}
 			}
 		}
 
@@ -111,10 +107,35 @@
 		"ja": { name: "Japanese", icon: "flag-jp" },
 		"jv": { name: "Javanese", icon: "flag-id" },
 		"kk": { name: "Kazakh", icon: "flag-kz" },
-
-		// todo: finish the list
-
-		"sk": { name: "Slovak", icon: "flag-sk" }
+		"ky": { name: "Kyrgyz", icon: "flag-kg" },
+		"ko": { name: "Korean", icon: "flag-kr" },
+		"la": { name: "Latin", icon: "flag-va" },
+		"lb": { name: "Luxembourgish", icon: "flag-lu" },
+		"lt": { name: "Lithuanian", icon: "flag-lt" },
+		"lv": { name: "Latvian", icon: "flag-lv" },
+		"mk": { name: "Macedonian", icon: "flag-mk" },
+		"ms": { name: "Malay", icon: "flag-my" },
+		"mt": { name: "Maltese", icon: "flag-mt" },
+		"mn": { name: "Mongolian", icon: "flag-mn" },
+		"ne": { name: "Nepali", icon: "flag-np" },
+		"no": { name: "Norwegian", icon: "flag-no" },
+		"fa": { name: "Persian", icon: "flag-ir" },
+		"pl": { name: "Polish", icon: "flag-pl" },
+		"pt": { name: "Portuguese", icon: "flag-pt" },
+		"ro": { name: "Romanian", icon: "flag-ro" },
+		"ru": { name: "Russian", icon: "flag-ru" },
+		"sr": { name: "Serbian", icon: "flag-rs" },
+		"sk": { name: "Slovak", icon: "flag-sk" },
+		"sl": { name: "Slovene", icon: "flag-si" },
+		"es": { name: "Spanish", icon: "flag-es" },
+		"sv": { name: "Swedish", icon: "flag-se" },
+		"tg": { name: "Tajik", icon: "flag-tj" },
+		"th": { name: "Thai", icon: "flag-th" },
+		"tk": { name: "Turkmen", icon: "flag-tm" },
+		"tr": { name: "Turkish", icon: "flag-tr" },
+		"uk": { name: "Ukrainian", icon: "flag-ua" },
+		"uz": { name: "Uzbek", icon: "flag-uz" },
+		"vi": { name: "Vietnamese", icon: "flag-vn" }
 	};
 
 })(jQuery);
